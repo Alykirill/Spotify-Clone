a0043d97051f2eb0bf55b15c965dffcd8448e5c2
kirilleitan <kirilleitan@gmail.com>
2020-10-02 16:06:45 +0300

    add spotify api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Login from "./components/Login/Login";
+import {getTokenFromUrl} from "./api/spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./components/Player/Player";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+    const [token, setToken] = useState(null)
+
+    useEffect(() => {
+        const hash = getTokenFromUrl();
+        window.location.hash = "";
+        const _token = hash.access_token;
+
+        if (_token) {
+            setToken(_token)
+            spotify.setAccessToken(_token);
+            spotify.getMe().then(user => {
+                console.log("Person: ", user)
+            })
+        }
+
+        console.log('A have a token', token);
+    }, [])
+
     return (
         <div className="app">
-            <h1>Hello Spotify</h1>
+            {
+                token ? <Player/>
+                    :
+                    <Login/>
+
+            }
+
         </div>
     );
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Login.css'
+import {loginUrl} from "../../api/spotify";
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className="login">
+            <h1>Im the login page</h1>
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
+            <a href={loginUrl}>Login with Spotify</a>
         </div>
     );
 };
