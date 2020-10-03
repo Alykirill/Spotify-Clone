7025bb7f4812e630ac9ca3076d169911d0eac9ad
kirilleitan <kirilleitan@gmail.com>
2020-10-03 23:49:44 +0300

    add Footer functions

--- src/components/Player/Footer/Footer.js
+++ src/components/Player/Footer/Footer.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import './Footer.css'
+import React, { useEffect, useState } from "react";
+//import { useStateValue } from "./StateProvider";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import "./Footer.css";
+import { Grid, Slider } from "@material-ui/core";
 
 const Footer = () => {
     return (
         <div className="footer">
             <div className="footer__left">
-                <p>Albom and song details</p>
+                <img className="footer__albumLogo" src="https://i.scdn.co/image/ab67616d00004851c380c82f7dfca69200a92dc2" alt=""/>
+                <div className="footer__songInfo">
+                    <h4>Yeah!</h4>
+                    <p>Usher</p>
+                </div>
             </div>
+
             <div className="footer__center">
-                <p>Player controls</p>
+                <ShuffleIcon className="footer__green"/>
+                <SkipPreviousIcon className="footer__icon"/>
+                <PlayCircleOutlineIcon fontSize="large" className="footer__icon"/>
+                <SkipNextIcon className="footer__icon"/>
+                <RepeatIcon className="footer__green"/>
+
             </div>
+
             <div className="footer__right">
-                <p>Volume controls</p>
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon/>
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Slider/>
+                    </Grid>
+                </Grid>
             </div>
 
         </div>
